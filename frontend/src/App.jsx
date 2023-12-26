ea8f348f88dbff03f2102fb8b91337c75001d321
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-12-25 23:07:29 -0700

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,21 @@ import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
-const App = () => {
-  return (
-    <div className="App">
-      <PhotoListItem/>
-    </div>
-  );
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
+
+ const App = () => (
+   <div className="App">
+     {/* The rest of your React code */}
+   </div>
+ )
 
 export default App;
